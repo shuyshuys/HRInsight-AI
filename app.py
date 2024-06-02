01c4d76621f91c1c7a5d9fde8d9dd401361d0182
shuyshuys <ivlyns@protonmail.com>
2024-06-03 00:16:08 +0700

    init

---
+++ app.py
@@ -0,0 +1,317 @@
+from flask import Flask, request, render_template
+from PyPDF2 import PdfReader
+import re
+import pickle
+from kbbi import KBBI
+
+app = Flask(__name__)
+
+# Load model
+rf_classifier_categorization = pickle.load(open('models/rf_classifier_categorization.pkl', 'rb'))
+tfidf_vectorizer_categorization = pickle.load(open('models/tfidf_vectorizer_categorization.pkl', 'rb'))
+rf_classifier_job_recommendation = pickle.load(open('models/rf_classifier_job_recommendation.pkl', 'rb'))
+tfidf_vectorizer_job_recommendation = pickle.load(open('models/tfidf_vectorizer_job_recommendation.pkl', 'rb'))
+
+# Clean resume
+def clean_resume(txt):
+    cleanText = re.sub('http\S+\s', ' ', txt)
+    cleanText = re.sub('RT|cc', ' ', cleanText)
+    cleanText = re.sub('#\S+\s', ' ', cleanText)
+    cleanText = re.sub('@\S+', '  ', cleanText)
+    cleanText = re.sub('[%s]' % re.escape("""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""), ' ', cleanText)
+    cleanText = re.sub(r'[^\x00-\x7f]', ' ', cleanText)
+    cleanText = re.sub('\s+', ' ', cleanText)
+    return cleanText
+
+# Prediksi dan Nama Kategori
+def prediksi_kategori(resume_text):
+    resume_text = clean_resume(resume_text)
+    resume_tfidf = tfidf_vectorizer_categorization.transform([resume_text])
+    predicted_category = rf_classifier_categorization.predict(resume_tfidf)[0]
+    return predicted_category
+
+# Prediction and Category Name
+def rekomendasi_job(resume_text):
+    resume_text= clean_resume(resume_text)
+    resume_tfidf = tfidf_vectorizer_job_recommendation.transform([resume_text])
+    recommended_job = rf_classifier_job_recommendation.predict(resume_tfidf)[0]
+    return recommended_job
+
+def pdf_to_text(file):
+    reader = PdfReader(file)
+    text = ''
+    for page in range(len(reader.pages)):
+        text += reader.pages[page].extract_text()
+    return text
+
+
+
+
+# resume parsing
+import re
+
+def ekstrak_no_telp(text):
+    contact_number = None
+
+    # Use regex pattern to find a potential contact number
+    #pattern = r"\b(?:\+?\d{1,3}[-.\s]?)?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}\b"
+    pattern = r'(\+62\s?[\d\s-]{10,13}|0[\d\s-]{10,13})'
+    match = re.search(pattern, text)
+    if match:
+        contact_number = match.group()
+
+    return contact_number
+def ekstrak_email(text):
+    email = None
+
+    # Use regex pattern to find a potential email address
+    pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b"
+    match = re.search(pattern, text)
+    if match:
+        email = match.group()
+
+    return email
+
+def ektrak_skill(text):
+    # List of predefined skills
+    skills_list = [
+        'Nuxt', 'Next.js', 'Vue', 'Vue.js', 'Bootstrap', 'Kotlin', 'Golang', 'Code Igniter', 'CodeIgniter', 'Python', 'Data Analysis', 'Machine Learning', 'Communication', 'Project Management', 'Deep Learning', 'SQL',
+        'Tableau',
+        'Java', 'C++', 'JavaScript', 'HTML', 'CSS', 'React', 'Angular', 'Node.js', 'MongoDB', 'Express.js', 'Git',
+        'Research', 'Statistics', 'Quantitative Analysis', 'Qualitative Analysis', 'SPSS', 'R', 'Data Visualization',
+        'Matplotlib',
+        'Seaborn', 'Plotly', 'Pandas', 'Numpy', 'Scikit-learn', 'TensorFlow', 'Keras', 'PyTorch', 'NLTK', 'Text Mining',
+        'Natural Language Processing', 'Computer Vision', 'Image Processing', 'OCR', 'Speech Recognition',
+        'Recommendation Systems',
+        'Collaborative Filtering', 'Content-Based Filtering', 'Reinforcement Learning', 'Neural Networks',
+        'Convolutional Neural Networks',
+        'Recurrent Neural Networks', 'Generative Adversarial Networks', 'XGBoost', 'Random Forest', 'Decision Trees',
+        'Support Vector Machines',
+        'Linear Regression', 'Logistic Regression', 'K-Means Clustering', 'Hierarchical Clustering', 'DBSCAN',
+        'Association Rule Learning',
+        'Apache Hadoop', 'Apache Spark', 'MapReduce', 'Hive', 'HBase', 'Apache Kafka', 'Data Warehousing', 'ETL',
+        'Big Data Analytics',
+        'Cloud Computing', 'Amazon Web Services (AWS)', 'Microsoft Azure', 'Google Cloud Platform (GCP)', 'Docker',
+        'Kubernetes', 'Linux',
+        'Shell Scripting', 'Cybersecurity', 'Network Security', 'Penetration Testing', 'Firewalls', 'Encryption',
+        'Malware Analysis',
+        'Digital Forensics', 'CI/CD', 'DevOps', 'Agile Methodology', 'Scrum', 'Kanban', 'Continuous Integration',
+        'Continuous Deployment',
+        'Software Development', 'Web Development', 'Mobile Development', 'Backend Development', 'Frontend Development',
+        'Full-Stack Development',
+        'UI/UX Design', 'Responsive Design', 'Wireframing', 'Prototyping', 'User Testing', 'Adobe Creative Suite',
+        'Photoshop', 'Illustrator',
+        'InDesign', 'Figma', 'Sketch', 'Zeplin', 'InVision', 'Product Management', 'Market Research',
+        'Customer Development', 'Lean Startup',
+        'Business Development', 'Sales', 'Marketing', 'Content Marketing', 'Social Media Marketing', 'Email Marketing',
+        'SEO', 'SEM', 'PPC',
+        'Google Analytics', 'Facebook Ads', 'LinkedIn Ads', 'Lead Generation', 'Customer Relationship Management (CRM)',
+        'Salesforce',
+        'HubSpot', 'Zendesk', 'Intercom', 'Customer Support', 'Technical Support', 'Troubleshooting',
+        'Ticketing Systems', 'ServiceNow',
+        'ITIL', 'Quality Assurance', 'Manual Testing', 'Automated Testing', 'Selenium', 'JUnit', 'Load Testing',
+        'Performance Testing',
+        'Regression Testing', 'Black Box Testing', 'White Box Testing', 'API Testing', 'Mobile Testing',
+        'Usability Testing', 'Accessibility Testing',
+        'Cross-Browser Testing', 'Agile Testing', 'User Acceptance Testing', 'Software Documentation',
+        'Technical Writing', 'Copywriting',
+        'Editing', 'Proofreading', 'Content Management Systems (CMS)', 'WordPress', 'Joomla', 'Drupal', 'Magento',
+        'Shopify', 'E-commerce',
+        'Payment Gateways', 'Inventory Management', 'Supply Chain Management', 'Logistics', 'Procurement',
+        'ERP Systems', 'SAP', 'Oracle',
+        'Microsoft Dynamics', 'Tableau', 'Power BI', 'QlikView', 'Looker', 'Data Warehousing', 'ETL',
+        'Data Engineering', 'Data Governance',
+        'Data Quality', 'Master Data Management', 'Predictive Analytics', 'Prescriptive Analytics',
+        'Descriptive Analytics', 'Business Intelligence',
+        'Dashboarding', 'Reporting', 'Data Mining', 'Web Scraping', 'API Integration', 'RESTful APIs', 'GraphQL',
+        'SOAP', 'Microservices',
+        'Serverless Architecture', 'Lambda Functions', 'Event-Driven Architecture', 'Message Queues', 'GraphQL',
+        'Socket.io', 'WebSockets'
+                     'Ruby', 'Ruby on Rails', 'PHP', 'Symfony', 'Laravel', 'CakePHP', 'Zend Framework', 'ASP.NET', 'C#',
+        'VB.NET', 'ASP.NET MVC', 'Entity Framework',
+        'Spring', 'Hibernate', 'Struts', 'Kotlin', 'Swift', 'Objective-C', 'iOS Development', 'Android Development',
+        'Flutter', 'React Native', 'Ionic',
+        'Mobile UI/UX Design', 'Material Design', 'SwiftUI', 'RxJava', 'RxSwift', 'Django', 'Flask', 'FastAPI',
+        'Falcon', 'Tornado', 'WebSockets',
+        'GraphQL', 'RESTful Web Services', 'SOAP', 'Microservices Architecture', 'Serverless Computing', 'AWS Lambda',
+        'Google Cloud Functions',
+        'Azure Functions', 'Server Administration', 'System Administration', 'Network Administration',
+        'Database Administration', 'MySQL', 'PostgreSQL',
+        'SQLite', 'Microsoft SQL Server', 'Oracle Database', 'NoSQL', 'MongoDB', 'Cassandra', 'Redis', 'Elasticsearch',
+        'Firebase', 'Google Analytics',
+        'Google Tag Manager', 'Adobe Analytics', 'Marketing Automation', 'Customer Data Platforms', 'Segment',
+        'Salesforce Marketing Cloud', 'HubSpot CRM',
+        'Zapier', 'IFTTT', 'Workflow Automation', 'Robotic Process Automation (RPA)', 'UI Automation',
+        'Natural Language Generation (NLG)',
+        'Virtual Reality (VR)', 'Augmented Reality (AR)', 'Mixed Reality (MR)', 'Unity', 'Unreal Engine', '3D Modeling',
+        'Animation', 'Motion Graphics',
+        'Game Design', 'Game Development', 'Level Design', 'Unity3D', 'Unreal Engine 4', 'Blender', 'Maya',
+        'Adobe After Effects', 'Adobe Premiere Pro',
+        'Final Cut Pro', 'Video Editing', 'Audio Editing', 'Sound Design', 'Music Production', 'Digital Marketing',
+        'Content Strategy', 'Conversion Rate Optimization (CRO)',
+        'A/B Testing', 'Customer Experience (CX)', 'User Experience (UX)', 'User Interface (UI)', 'Persona Development',
+        'User Journey Mapping', 'Information Architecture (IA)',
+        'Wireframing', 'Prototyping', 'Usability Testing', 'Accessibility Compliance', 'Internationalization (I18n)',
+        'Localization (L10n)', 'Voice User Interface (VUI)',
+        'Chatbots', 'Natural Language Understanding (NLU)', 'Speech Synthesis', 'Emotion Detection',
+        'Sentiment Analysis', 'Image Recognition', 'Object Detection',
+        'Facial Recognition', 'Gesture Recognition', 'Document Recognition', 'Fraud Detection',
+        'Cyber Threat Intelligence', 'Security Information and Event Management (SIEM)',
+        'Vulnerability Assessment', 'Incident Response', 'Forensic Analysis', 'Security Operations Center (SOC)',
+        'Identity and Access Management (IAM)', 'Single Sign-On (SSO)',
+        'Multi-Factor Authentication (MFA)', 'Blockchain', 'Cryptocurrency', 'Decentralized Finance (DeFi)',
+        'Smart Contracts', 'Web3', 'Non-Fungible Tokens (NFTs)']
+
+
+    skills = []
+
+    for skill in skills_list:
+        pattern = r"\b{}\b".format(re.escape(skill))
+        match = re.search(pattern, text, re.IGNORECASE)
+        if match:
+            skills.append(skill)
+
+    return skills
+
+
+def ekstrak_edukasi(text):
+    education = []
+
+    # List of education keywords to match against
+    education_keywords = [
+        'Computer Science', 'Information Technology', 'Software Engineering', 'Electrical Engineering', 'Mechanical Engineering', 'Civil Engineering',
+        'Chemical Engineering', 'Biomedical Engineering', 'Aerospace Engineering', 'Nuclear Engineering', 'Industrial Engineering', 'Systems Engineering',
+        'Environmental Engineering', 'Petroleum Engineering', 'Geological Engineering', 'Marine Engineering', 'Robotics Engineering', 'Biotechnology',
+        'Biochemistry', 'Microbiology', 'Genetics', 'Molecular Biology', 'Bioinformatics', 'Neuroscience', 'Biophysics', 'Biostatistics', 'Pharmacology',
+        'Physiology', 'Anatomy', 'Pathology', 'Immunology', 'Epidemiology', 'Public Health', 'Health Administration', 'Nursing', 'Medicine', 'Dentistry',
+        'Pharmacy', 'Veterinary Medicine', 'Medical Technology', 'Radiography', 'Physical Therapy', 'Occupational Therapy', 'Speech Therapy', 'Nutrition',
+        'Sports Science', 'Kinesiology', 'Exercise Physiology', 'Sports Medicine', 'Rehabilitation Science', 'Psychology', 'Counseling', 'Social Work',
+        'Sociology', 'Anthropology', 'Criminal Justice', 'Political Science', 'International Relations', 'Economics', 'Finance', 'Accounting', 'Business Administration',
+        'Management', 'Marketing', 'Entrepreneurship', 'Hospitality Management', 'Tourism Management', 'Supply Chain Management', 'Logistics Management',
+        'Operations Management', 'Human Resource Management', 'Organizational Behavior', 'Project Management', 'Quality Management', 'Risk Management',
+        'Strategic Management', 'Public Administration', 'Urban Planning', 'Architecture', 'Interior Design', 'Landscape Architecture', 'Fine Arts',
+        'Visual Arts', 'Graphic Design', 'Fashion Design', 'Industrial Design', 'Product Design', 'Animation', 'Film Studies', 'Media Studies',
+        'Communication Studies', 'Journalism', 'Broadcasting', 'Creative Writing', 'English Literature', 'Linguistics', 'Translation Studies',
+        'Foreign Languages', 'Modern Languages', 'Classical Studies', 'History', 'Archaeology', 'Philosophy', 'Theology', 'Religious Studies',
+        'Ethics', 'Education', 'Early Childhood Education', 'Elementary Education', 'Secondary Education', 'Special Education', 'Higher Education',
+        'Adult Education', 'Distance Education', 'Online Education', 'Instructional Design', 'Curriculum Development'
+        'Library Science', 'Information Science', 'Computer Engineering', 'Software Development', 'Cybersecurity', 'Information Security',
+        'Network Engineering', 'Data Science', 'Data Analytics', 'Business Analytics', 'Operations Research', 'Decision Sciences',
+        'Human-Computer Interaction', 'User Experience Design', 'User Interface Design', 'Digital Marketing', 'Content Strategy',
+        'Brand Management', 'Public Relations', 'Corporate Communications', 'Media Production', 'Digital Media', 'Web Development',
+        'Mobile App Development', 'Game Development', 'Virtual Reality', 'Augmented Reality', 'Blockchain Technology', 'Cryptocurrency',
+        'Digital Forensics', 'Forensic Science', 'Criminalistics', 'Crime Scene Investigation', 'Emergency Management', 'Fire Science',
+        'Environmental Science', 'Climate Science', 'Meteorology', 'Geography', 'Geomatics', 'Remote Sensing', 'Geoinformatics',
+        'Cartography', 'GIS (Geographic Information Systems)', 'Environmental Management', 'Sustainability Studies', 'Renewable Energy',
+        'Green Technology', 'Ecology', 'Conservation Biology', 'Wildlife Biology', 'Zoology', 'Teknik Informatika', 'Sistem Informasi', 'Ilmu Komputer', 'Manajemen Informatika', 'Teknik Elektro', 
+        'Teknik Mesin', 'Teknik Sipil', 'Arsitektur', 'Desain Interior', 'Desain Komunikasi Visual', 
+        'Ilmu Komunikasi', 'Hubungan Internasional', 'Hukum', 'Akuntansi', 'Manajemen', 'Ekonomi', 
+        'Psikologi', 'Sosiologi', 'Antropologi', 'Pendidikan', 'Sastra', 'Biologi', 'Fisika', 'Kimia', 
+        'Matematika', 'Farmasi', 'Kedokteran', 'Keperawatan', 'Ilmu Gizi', 'Agribisnis', 'Agroteknologi', 
+        'Teknologi Pangan', 'Peternakan', 'Kehutanan', 'Pariwisata', 'Perhotelan', 'Teknik Lingkungan', 
+        'Teknik Industri', 'Teknik Kimia', 'Teknologi Informasi']
+
+    for keyword in education_keywords:
+        pattern = r"(?i)\b{}\b".format(re.escape(keyword))
+        match = re.search(pattern, text)
+        if match:
+            education.append(match.group())
+
+    return education
+
+def extract_first_sentence(text):
+    # Split text into sentences
+    sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', text)
+    # Return the first sentence
+    return sentences[0]
+
+# Fungsi untuk memeriksa apakah kata ada di KBBI
+def is_in_kbbi(word):
+    try:
+        KBBI(word)
+        return True
+    except:
+        return False
+
+# def ekstrak_nama(text):
+    # Extract the first sentence
+    # first_sentence = text
+    # Regular expression patterns for detecting names
+    # pattern_all_uppercase = r"\b[A-Z][A-Z]+\b"
+    # pattern_first_uppercase = r"\b[A-Z][a-z]+\b"
+    # name = None
+
+    # Use regex pattern to find a potential name
+    # pattern = r"(\b[A-Z][a-z]+\b)\s(\b[A-Z][a-z]+\b)"
+    # pattern = r"\b[A-Z][a-z]+(?: [A-Z][a-z]+)?\b"
+    # pattern = r"\b[A-Z][a-z]+(?: [A-Z][a-z]+)?(?: [A-Z][a-z]+)?\b"
+    # pattern = r"\b[A-Z]{2,4}(?:\s[A-Z]{2,4}){1,3}\b"
+    # pattern = r"\b[A-Z][a-z]+(?:\s[A-Z][a-z]+){1,3}\b"
+
+    # match = re.search(pattern, text)
+    # if match:
+    #     name = match.group()
+
+    # Find potential names in the first sentence
+    # matches_all_uppercase = re.findall(pattern_all_uppercase, first_sentence)
+    # matches_first_uppercase = re.findall(pattern_first_uppercase, first_sentence)
+    
+    # Combine potential names from both patterns
+    # potential_names = matches_all_uppercase + matches_first_uppercase
+
+    # names = [potential_names for potential_names in potential_names if not is_in_kbbi(potential_names)]
+    
+    # return names
+
+    # return text
+
+
+
+
+# routes===============================================
+
+@app.route('/')
+def resume():
+    # Provide a simple UI to upload a resume
+    return render_template("index.html")
+
+# @app.route('/hope', methods=['GET'])
+# def hope():
+#     return render_template("form.html")
+
+@app.route('/privacy-policy')
+def privacy_policy():
+    return render_template('privacy-policy.html')
+
+@app.route('/tos')
+def terms_of_use():
+    return render_template('tos.html')
+
+@app.route('/prediksi', methods=['POST'])
+def pred():
+    # Process the PDF or TXT file and make prediction
+    if 'resume' in request.files:
+        file = request.files['resume']
+        filename = file.filename
+        if filename.endswith('.pdf'):
+            text = pdf_to_text(file)
+        elif filename.endswith('.txt'):
+            text = file.read().decode('utf-8')
+        else:
+            return render_template('index.html', message="Invalid file format. Please upload a PDF or TXT file.")
+
+        predicted_category = prediksi_kategori(text)
+        recommended_job = rekomendasi_job(text)
+        phone = ekstrak_no_telp(text)
+        email = ekstrak_email(text)
+
+        extracted_skills = ektrak_skill(text)
+        extracted_education = ekstrak_edukasi(text)
+
+        return render_template('index.html', predicted_category=predicted_category,recommended_job=recommended_job,
+                               phone=phone,email=email,extracted_skills=extracted_skills,extracted_education=extracted_education)
+    else:
+        return render_template("index.html", message="No resume file uploaded.")
+
+if __name__ == '__main__':
+    app.run(debug=True)
